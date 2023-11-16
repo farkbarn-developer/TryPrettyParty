d62e4fc741107cc939d5ccfb99ddc9e1e7a47f00
Frank Barrera <frankbarreraag@gmail.com>
2023-11-16 13:28:06 -0500

    agregando sitio

---
+++ TryPrettyParty/checkout.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Pretty Party</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="author" content="Instant Profits Media" />
+  <meta keywords="hidden,beauty,celebrity,pretty,party,Doctors,medicine"/>
+  <meta name="description" content="Celebrity Singer Ponytail Secret is Exposed" />
+  <meta name="robots" content="index,follow" />
+  <link rel="shortcut icon" type="image/png" href="img/indice.png">
+  <link rel="stylesheet" href="css/style.css" />
+  <link rel="stylesheet" href="css/responsive.css" />
+  <script src="animateprogress.js"></script>
+</head>
+<body>
+
+<header class="header">
+	<section class="lfheader">
+		<img src="img/logo.png" alt="">
+		<p class="txtlogo">Advertorial</p>
+	</section>
+</header>
+
+<section class="center">
+  <p class="pord">Order Now</p>
+  <p class="pyou">You've made a wise decision to get started with Stricition!</p>
+  <p class="pcon">confirm your delivery address and enter your payment information (in the form to the right) <br>to get your order processed and underway to you immediately.</p>
+  <p class="pfre">Free Shipping on all orders Today!</p>
+  <p class="pdon">Do not browse away from this page!</p>
+  <p class="pfre">Free delivery available today</p>
+  <p class="pint">Internet Exclusive Offer Available to U.S. Residents Only</p>
+  <p class="pdis">DISCOUNTED BOTTLE OFFER</p>
+  <p class="plim">LIMITED TIME ONLY</p>
+  <p>Helps to balance blood sugar levels.</p>
+  <p>Helps to promote balance in blood pressure.</p>
+  <p>Helps to paces up metabolism.</p>
+  <p>Helps to strengthens the heart.</p>
+  <p>Helps to maintain blood sugar levels.</p>
+  <p>Help to generates energy.</p>
+  <p>100% SATISFACTION GUARANTEE</p>
+  <p>RUSH MY ORDER >></p>
+  <p>Safe 256-Bit SSL encryption.</p>
+</section>
+
+<footer class="footer">
+<section class="containerfooter">
+	<img src="img/logo.png" alt="">
+	<p class="pter">
+		<script type="text/javascript">
+				copyright=new Date();
+				update=copyright.getFullYear();
+				document.write(" © " + update + " " + "Pretty Party ");
+		</script>
+		 <br> Onnit Labs, Inc.</p>
+	<p class="prep">Representations regarding the efficacy and safety of Pretty Party have not been evaluated by the Food and Drug Administration. The FDA only evaluates foods and drugs, not supplements like these products. These products are not intended to diagnose, prevent, treat, or cure any disease. This information does not constitute medical advice and it should not be relied upon as such. Consult with your doctor before modifying your regular medical regime.
+	<p class="psou">Sources:</p>
+	<p class="pitemfo">1. <a href="https://time.com/3858309/attention-spans-goldfish/">https://time.com/3858309/attention-spans-goldfish/</a></p>
+	<p class="pitemfo">2. <a href="https://bigthink.com/personal-growth/what-is-the-flow-state">https://bigthink.com/personal-growth/what-is-the-flow-state</a></p>
+	<p class="pitemfo">3. <a href="https://health.usnews.com/wellness/mind/articles/2018-09-27/what-are-nootropics">https://health.usnews.com/wellness/mind/articles/2018-09-27/what-are-nootropics</a></p>
+	<p class="pitemfo">4. <a href="https://www.ncbi.nlm.nih.gov/pubmed/26876224">https://www.ncbi.nlm.nih.gov/pubmed/26876224</a></p>
+	<p class="pitemfo">5. <a href="https://www.onnit.com/files/AAIC_ERP.pdf">https://www.onnit.com/files/AAIC_ERP.pdf</a></p>
+	<p class="pitemfo">6. <a href="https://www.bscg.org/">https://www.bscg.org/</a></p>
+	<p class="pitemfo">7. <a href="https://www.rxlist.com/adderall-side-effects-drug-center.htm">https://www.rxlist.com/adderall-side-effects-drug-center.htm</a></p>
+	<br><br>
+	<p class="pdisc">*THIS IS AN ADVERTISEMENT AND NOT AN ACTUAL NEWS ARTICLE, BLOG, OR CONSUMER PROTECTION UPDATE* <br>
+ATTENTION: ALL CONTENT AND INFORMATION FOUND IN THE PRETTY PARTY WEBSITE AND AFFILIATED LINKS (COLLECTIVELY REFERRED TO HEREIN AS "WE", “US” OR "OUR") ARE FOR INFORMATIONAL PURPOSES ONLY AND ARE STRICTLY NOT INTENDED TO DIAGNOSE, TREAT, CURE OR PREVENT ANY DISEASE, FOOT PROBLEM OR ANY OTHER MEDICAL CONDITION. YOU SHOULD ALWAYS CONSULT WITH YOUR HEALTHCARE PROVIDER BEFORE USING OUR PRODUCT IN CONJUNCTION WITH EXISTING HEALTH CONDITIONS.
+Individual results may vary which is quite normal for anything. The FDA hasn't evaluated the statements provided on this page. PRETTY PARTY Review isn't supposed to prevent or diagnose and treat any disease. Finally, make sure you consult with a licensed doctor before taking any supplement or making any changes to your diet or exercise plan.
+www.tryprettyparty.com is a participant in the Alpha Brain Associates Program, an affiliate advertising program designed to provide a way for web owners to gain advertising fees by advertising and linking to Pretty Party website.
+Testimonials and comments found within our website are unverified results that have been sent to us by appreciative customers, which are representative of typical or average consumers, and are not intended to guarantee that everyone will achieve the exact same results.
+Our website content is provided as a service; all content is solely for informational purpose only.
+MARKETING DISCLOSURE: This website is a market place. As such you should know that the owner has a monetary connection to the product and services advertised on the site. The owner receives payment whenever a qualified lead is referred but that is the extent of it.
+ADVERTISING DISCLOSURE: This website and the products & services referred to on the site are advertising marketplaces. This website is an advertisement and not a news publication. Any photographs of persons used on this site are models. The owner of this site and of the products and services referred to on this site only provides a service where consumers can obtain and compare.
+<section class="terms">
+	<a href="#popuptercon">Terms & Conditions</a> | <a href="#popuppri">Privacy Policy</a> | <a href="#popupcon">Contact Us</a>
+
+	<div id="popuptercon" class="overlay">
+			<div id="popupBody">
+					<h2>Terms & Conditions</h2>
+					<a id="cerrar" href="#">&times;</a>
+					<div class="popupContent">
+							<p>
+<br>1. Terms<br>
+We are an affiliate marketing company.<br>
+By accessing this web site, you are agreeing to be bound by these web site Terms and Conditions of Use, all applicable laws and regulations, and agree that you are responsible for compliance with any applicable local laws. If you do not agree with any of these terms, you are prohibited from using or accessing this site. The materials contained in this web site are protected by applicable copyright and trade mark law.
+<br><br>2. Use License<br>
+Permission is granted to temporarily download one copy of the materials (information or software) on www.tryprettyparty.com web site for personal, non-commercial transitory viewing only. This is the grant of a license, not a transfer of title, and under this license you may not: modify or copy the materials; use the materials for any commercial purpose, or for any public display (commercial or non-commercial); attempt to decompile or reverse engineer any software contained on myeasytv.com’s web site; remove any copyright or other proprietary notations from the materials; or transfer the materials to another person or “mirror” the materials on any other server.
+<br>This license shall automatically terminate if you violate any of these restrictions and may be terminated by www.tryprettyparty.com at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+<br><br>3. Disclaimer<br>
+The materials on www.tryprettyparty.com web site are provided “as is”. www.tryprettyparty.com makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, www.tryprettyparty.com does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
+<br><br>4. Limitations<br>
+In no event shall www.tryprettyparty.com or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on www.tryprettyparty.com Internet site, even if www.tryprettyparty.com or a www.tryprettyparty.com authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+<br><br>5. Revisions and Errata<br>
+The materials appearing on www.tryprettyparty.com web site could include technical, typographical, or photographic errors. www.tryprettyparty.com does not warrant that any of the materials on its web site are accurate, complete, or current. www.tryprettyparty.com may make changes to the materials contained on its web site at any time without notice. www.tryprettyparty.com does not, however, make any commitment to update the materials.
+<br><br>6. Links<br>
+www.tryprettyparty.com has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by www.tryprettyparty.com of the site. Use of any such linked web site is at the user’s own risk.
+<br><br>7. Site Terms of Use Modifications<br>
+www.tryprettyparty.com may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
+<br><br>8. Governing Law<br>
+Any claim relating to www.tryprettyparty.com web site shall be governed by the laws of USA.
+General Terms and Conditions applicable to Use of a Web Site.
+To contact us please send an email to Info at www.tryprettyparty.com
+							</p>
+					</div>
+			</div>
+	</div>
+
+	<div id="popuppri" class="overlay">
+			<div id="popupBody">
+					<h2>Privacy Policy</h2>
+					<a id="cerrar" href="#">&times;</a>
+					<div class="popupContent">
+							<p>
+								Privacy Policy for www.tryprettyparty.com
+								The privacy of our visitors to www.tryprettyparty.com is important to us.
+								At www.tryprettyparty.com, we recognize that privacy of your personal information is important. Here is information on what types of personal information we receive and collect when you use and visit www.tryprettyparty.com, and how we safeguard your information. We never sell your personal information to third parties.
+								Log Files
+								As with most other websites, we collect and use the data contained in log files. The information in the log files include your IP (internet protocol) address, your ISP (internet service provider, such as AOL or Shaw Cable), the browser you used to visit our site (such as Internet Explorer or Firefox), the time you visited our site and which pages you visited throughout our site.
+								Cookies and Web Beacons
+								We do use cookies to store information, such as your personal preferences when you visit our site. This could include only showing you a popup once in your visit, or the ability to login to some of our features, such as forums.
+								We also use third party advertisements on www.tryprettyparty.com to support our site. Some of these advertisers may use technology such as cookies and web beacons when they advertise on our site, which will also send these advertisers (such as Google through the Google AdSense program) information including your IP address, your ISP , the browser you used to visit our site, and in some cases, whether you have Flash installed. This is generally used for geotargeting purposes (showing New York real estate ads to someone in New York, for example) or showing certain ads based on specific sites visited (such as showing cooking ads to someone who frequents cooking sites).
+								DoubleClick DART cookies
+								We also may use DART cookies for ad serving through Google’s DoubleClick, which places a cookie on your computer when you are browsing the web and visit a site using DoubleClick advertising (including some Google AdSense advertisements). This cookie is used to serve ads specific to you and your interests (”interest based targeting”). The ads served will be targeted based on your previous browsing history (For example, if you have been viewing sites about visiting Las Vegas, you may see Las Vegas hotel advertisements when viewing a non-related site, such as on a site about hockey). DART uses “non personally identifiable information”. It does NOT track personal information about you, such as your name, email address, physical address, telephone number, social security numbers, bank account numbers or credit card numbers. You can opt-out of this ad serving on all sites using this advertising by visiting http://www.doubleclick.com/privacy/dart_adserving.aspx
+								You can chose to disable or selectively turn off our cookies or third-party cookies in your browser settings, or by managing preferences in programs such as Norton Internet Security. However, this can affect how you are able to interact with our site as well as other websites. This could include the inability to login to services or programs, such as logging into forums or accounts.
+								Deleting cookies does not mean you are permanently opted out of any advertising program. Unless you have settings that disallow cookies, the next time you visit a site running the advertisements, a new cookie will be added.
+								www.tryprettyparty.com does not host online television shows or sports. We are an affiliate website that provides you with reviews of other 3rd party software.We have tested all the software and services recommended on this site.
+								We are an affiliate marketing company.
+								We may get commissions for referring you to some of the services we recommend on this site.
+								To contact us please send an email to info at www.tryprettyparty.com
+							</p>
+					</div>
+			</div>
+	</div>
+
+	<div id="popupcon" class="overlay">
+			<div id="popupBody">
+					<h2>Contact Us</h2>
+					<a id="cerrar" href="#">&times;</a>
+					<div class="popupContent">
+							<p>
+								To contact us please send an email to info at contact@tryprettyparty.com
+							</p>
+					</div>
+			</div>
+	</div>
+</section>
+</p>
+</section>
+</footer>
+</body>
+</html>
